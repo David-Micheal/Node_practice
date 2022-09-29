86f5ed125d8f9e18ce93f66dbc1654cfd9c5dc8a
David-Micheal <michealodebode2016@gmail.com>
2022-09-29 09:58:07 +0100

    node-class

---
+++ node.ts
@@ -0,0 +1,15 @@
+const display = (a: number, b: number): number => {
+  return a + b;
+};
+
+// setInterval(() => {
+//   console.log(1, 2);
+// }, 1000);
+
+setTimeout(() => {
+  console.log("Its time out.....");
+}),
+  1000;
+
+import os from "os";
+console.log(os);
